26b97f283f7292ecc035975f1b9ad79c4f9ece4f
chupino <mauricioescalante903@gmail.com>
2024-09-08 22:24:46 -0500

    a

--- run.sh
+++ run.sh
@@ -1,15 +1,11 @@
 git clone https://github.com/chupino/mctest.git mc
 cd mc
 
-sudo chown $user:$user server.properties
-
-docker build -t mctest .
+docker-compose up -d
 
 if [ $? -eq 0 ]; then
-	echo "Imagen construida 🍕 (noa chipi)"
+	echo "Desplegado 🍕 (noa chipi)"
 else
-	echo "La imagen falló (la mami de noa tiene la culpa)"
+	echo "El despliegue falló (la mami de noa tiene la culpa)"
 	exit 1
 fi
-
-docker run -dp 25565:25565 mctest
